863cb17bea70dffff1a5418cb3809cd14ec57fde
Diego Contreras <42013493+Desperationis@users.noreply.github.com>
2021-04-11 17:47:23 -0700

    Add youtube-dl, pygame, and pyautogui

--- install.bash
+++ install.bash
@@ -1,2 +1,4 @@
-sudo apt-get install python3 python3-pip git g++ cmake vim okular 
+sudo apt-get install python3 python3-pip git g++ cmake vim okular youtube-dl
 sudo apt-get install gstreamer1.0*
+
+pip3 install pygame pyautogui
