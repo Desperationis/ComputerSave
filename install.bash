43c9d89ec94b82228a1d172138e0de7189285fc7
Desperationis <smarttdiego@gmail.com>
2021-05-23 12:41:11 -0700

    Fix typo

--- install.bash
+++ install.bash
@@ -2,7 +2,7 @@ sudo apt-get install python3 python3-pip git g++ cmake cmake-curses-gui vim okul
 sudo apt-get install libc6-dev libc-dev-bin libncurses-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libssl-dev dos2unix
 sudo apt-get install gstreamer1.0*
 
-sudo apt-install build-essential
+sudo apt install build-essential
 
 pip3 install pygame pyautogui PythonFileLibrary psscraper
 
