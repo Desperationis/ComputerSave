36e6e34a46fc9e78f8b8d9b396833889c3aeea9a
Desperationis <smarttdiego@gmail.com>
2021-06-07 19:29:18 -0700

    Add gdb to packages

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
-sudo apt-get install python3 python3-pip git g++ cmake cmake-curses-gui vim okular youtube-dl keepass2 obs-studio xclip texlive-full youtube-dl tree python3-pip python3 npm nodejs gparted vlc
+sudo apt-get install python3 python3-pip git g++ cmake cmake-curses-gui vim okular youtube-dl keepass2 obs-studio xclip texlive-full youtube-dl tree python3-pip python3 npm nodejs gparted vlc gdb
 sudo apt-get install libc6-dev libc-dev-bin libncurses-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libssl-dev dos2unix
 sudo apt-get install gstreamer1.0* libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev pkg-config
 
