1de3f39fa7a106f3975fa34b791d42b8ef595a83
Desperationis <smarttdiego@gmail.com>
2021-11-27 08:59:55 -0800

    greatly improve install.bash and configureGit

--- configureGit.bash
+++ configureGit.bash
@@ -1,3 +1,12 @@
 # Make sure you don't must personal info in here lmao
 
 git config --global core.editor vim
+git config --global user.name "Desperationis"
+git config --global user.email "smarttdiego@gmail.com"
+
+if ! [[ -f ~/.ssh/id_ed25519 ]]
+then
+	ssh-keygen -t ed25519 -C "smarttdiego@gmail.com"
+	eval "$(ssh-agent -s)"
+	ssh-add ~/.ssh/id_ed25519
+fi
--- install.bash
+++ install.bash
@@ -1,12 +1,99 @@
-sudo apt-get install python3 git g++ cmake cmake-curses-gui vim okular youtube-dl keepass2 obs-studio xclip texlive-full youtube-dl tree python3-pip python3 npm nodejs gparted vlc gdb
-sudo apt-get install libc6-dev libc-dev-bin libncurses-dev libsdl2-dev libsdl2-image-dev libsdl2-ttf-dev libssl-dev dos2unix
-sudo apt-get install gstreamer1.0* libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev pkg-config
+install() {
+	echo "\n\n\nInstalling $1..."
+	if ! sudo apt-get install -y $1
+	then
+		echo "$1 was not a valid package."
+		exit 1
+	fi
+}
 
-sudo apt install build-essential python3-pip
+sudo apt-get update
+sudo apt-get -y upgrade
+
+install python3
+install python
+install git
+install g++
+install cmake
+install cmake-curses-gui
+install curl
+install wget
+
+if ! pix --version
+then
+	sudo add-apt-repository -y ppa:savoury1/xapps
+	sudo apt update
+	sudo apt install -y pix
+fi
+
+install vim
+install okular
+install keepass2
+install obs-studio
+install xclip
+install texlive-full
+install tree
+install python3
+install python3-pip
+install npm
+install nodejs
+install gparted
+install vlc
+install clementine
+install vlc
+install gdb
+install libc6-dev
+install libc-dev-bin
+install libncurses-dev
+install libsdl2-dev
+install libsdl2-ttf-dev
+install libsdl2-image-dev
+install libssl-dev
+install dos2unix
+
+if ! youtube-dl --version
+then
+	sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+	sudo chmod a+rx /usr/local/bin/youtube-dl
+fi
+
+if ! google-chrome-stable --version
+then
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+	install ./google-chrome-stable_current_amd64.deb
+	rm google-chrome-stable_current_amd64.deb
+fi
+
+if ! type discord
+then
+	wget -O ./discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+	install ./discord.deb
+	rm ./discord.deb
+fi
+
+if ! type anki
+then
+	(
+	wget -O anki.tar.bz2 https://github.com/ankitects/anki/releases/download/2.1.49/anki-2.1.49-linux.tar.bz2
+	tar -xf anki.tar.bz2
+	cd anki-2.1.49-linux/
+	chmod +x ./install.sh
+	sudo ./install.sh
+	)
+
+	rm anki.tar.bz2
+	rm -rf anki-2.1.49-linux
+fi
+
+
+sudo apt-get install -y gstreamer1.0*
+
+install libgstreamer-plugins-base1.0-dev
+install pkg-config
+install build-essential
+install python3-pip
 
 pip3 install pygame pyautogui PythonFileLibrary psscraper
-
-sudo apt-get upgrade
 
 # Misc:
 # JDownloader 2
