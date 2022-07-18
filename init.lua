cd15737b9d24999afab2b00e4cef2319ed4dd645
Diego Contreras <42013493+Desperationis@users.noreply.github.com>
2022-07-17 22:32:25 -0700

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,61 @@
+local set = vim.opt
+
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.linebreak = true
+set.backspace = "indent,eol,start"
+set.guicursor="n-v-c-sm:ver25,i-ci-ve:ver25,r-cr-o:hor20"
+
+require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+	use 'preservim/nerdtree'
+	use 'tpope/vim-fugitive'
+	use 'neovim/nvim-lspconfig'
+	use 'ms-jpq/coq_nvim'
+	use 'lewis6991/gitsigns.nvim'
+end)
+
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    -- Actions
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+    map('n', '<leader>hS', gs.stage_buffer)
+    map('n', '<leader>hu', gs.undo_stage_hunk)
+    map('n', '<leader>hR', gs.reset_buffer)
+    map('n', '<leader>hp', gs.preview_hunk)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+    map('n', '<leader>tb', gs.toggle_current_line_blame)
+    map('n', '<leader>hd', gs.diffthis)
+    map('n', '<leader>hD', function() gs.diffthis('~') end)
+    map('n', '<leader>td', gs.toggle_deleted)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
+
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.pyright.setup{}
