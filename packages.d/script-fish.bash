419d8019de70ce64c401d080839ccfc1f33e90d3
Desperationis <smarttdiego@gmail.com>
2022-07-22 23:06:05 -0700

    Update fish script

--- packages.d/script-fish.bash
+++ packages.d/script-fish.bash
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install fish
+if sudo apt-add-repository ppa:fish-shell/release-3
+then
+	sudo apt-get update
+	sudo apt-get install fish
+
+fi
+
+# Install plugins
+if ! which fisher
+then
+	fish -c "curl -sL https://git.io/fisher | source; fisher install jorgebucaran/fisher; fisher install edc/bass"
+fi
+
