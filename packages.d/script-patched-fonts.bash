f7a891519a78d6ca933ccc1d1e50a709695423c1
Desperationis <smarttdiego@gmail.com>
2022-07-25 18:17:44 -0700

    Update script-patched-fonts to install more

--- packages.d/script-patched-fonts.bash
+++ packages.d/script-patched-fonts.bash
@@ -2,7 +2,10 @@
 
 # Installation from https://github.com/neovim/neovim/releases/
 
-LINK="https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete.ttf"
+FONTS=(
+	https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete.ttf
+	https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono.ttf
+)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 FONT_PATH=$SCRIPT_DIR/../patched-fonts
 
@@ -11,12 +14,15 @@ if [ $REMOVE -eq 0 ]
 then
 	mkdir -p "$FONT_PATH"
 
-	if ! wget "$LINK" -P "$FONT_PATH"
-	then
-		echo -e "\033[0;31mERROR: Unable to download $LINK.\033[0m"
-		read ans
-		exit 1
-	fi
+	for LINK in ${FONTS[@]}
+	do
+		if ! wget "$LINK" -P "$FONT_PATH"
+		then
+			echo -e "\033[0;31mERROR: Unable to download $LINK.\033[0m"
+			read ans
+			exit 1
+		fi
+	done
 
 	echo -e "\033[0;92mPatched Fonts were downloaded to $FONT_PATH. Follow your specific distro's guide to install.\033[0m"
 fi
