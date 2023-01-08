0b924bfbba2f2af596999842df8acc658d054711
Desperationis <smarttdiego@gmail.com>
2023-01-07 22:17:02 -0800

    Move all packer setup code after warning msg

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,66 +9,6 @@ end
 
 
 
-local set = vim.opt
-
-set.tabstop = 4
-set.shiftwidth = 4
-set.softtabstop = 4
-set.linebreak = true
-set.backspace = "indent,eol,start"
-set.guicursor="n-v-c-sm:ver25,i-ci-ve:ver25,r-cr-o:hor20"
-
-function gitsigns()
-	require('gitsigns').setup{
-	  on_attach = function(bufnr)
-		local gs = package.loaded.gitsigns
-
-		local function map(mode, l, r, opts)
-		  opts = opts or {}
-		  opts.buffer = bufnr
-		  vim.keymap.set(mode, l, r, opts)
-		end
-
-		-- Navigation
-		map('n', ']c', function()
-		  if vim.wo.diff then return ']c' end
-		  vim.schedule(function() gs.next_hunk() end)
-		  return '<Ignore>'
-		end, {expr=true})
-
-		map('n', '[c', function()
-		  if vim.wo.diff then return '[c' end
-		  vim.schedule(function() gs.prev_hunk() end)
-		  return '<Ignore>'
-		end, {expr=true})
-
-		-- Actions
-		map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-		map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-		map('n', '<leader>hS', gs.stage_buffer)
-		map('n', '<leader>hu', gs.undo_stage_hunk)
-		map('n', '<leader>hR', gs.reset_buffer)
-		map('n', '<leader>hp', gs.preview_hunk)
-		map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-		map('n', '<leader>tb', gs.toggle_current_line_blame)
-		map('n', '<leader>hd', gs.diffthis)
-		map('n', '<leader>hD', function() gs.diffthis('~') end)
-		map('n', '<leader>td', gs.toggle_deleted)
-
-		-- Text object
-		map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-	  end
-	}
-end
-
-function lsp()
-	require("nvim-lsp-installer").setup{}
-	require'lspconfig'.clangd.setup{}
-	require'lspconfig'.pyright.setup{}
-	require'lspconfig'.bashls.setup{}
-end
-
-
 require('packer').startup(function()
 	use 'wbthomason/packer.nvim'
 	use 'preservim/nerdtree'
@@ -84,15 +24,6 @@ require('packer').startup(function()
 	end
 end)
 
-require("catppuccin").setup({
-	dim_inactive = {
-        enabled = true,
-        shade = "dark",
-        --percentage = 0.15,
-        percentage = 0.5,
-    },
-	term_colors=true
-})
 
 
 if is_bootstrap then
@@ -103,6 +34,15 @@ if is_bootstrap then
   print '=================================='
   return
 end
+
+
+-- Set Preferences
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.linebreak = true
+vim.opt.backspace = "indent,eol,start"
+vim.opt.guicursor="n-v-c-sm:ver25,i-ci-ve:ver25,r-cr-o:hor20"
 
 vim.o.mouse = ""
 vim.wo.number = true
@@ -116,6 +56,65 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   group = packer_group,
   pattern = vim.fn.expand '$MYVIMRC',
 })
+ 
 
-pcall(gitsigns)
-pcall(lsp)
+
+
+-- ANYTHING BELOW IS SETUP FOR ALL PACKAGES
+
+require("catppuccin").setup({
+	dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        --percentage = 0.15,
+        percentage = 0.5,
+    },
+	term_colors=true
+})
+
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+	local gs = package.loaded.gitsigns
+
+	local function map(mode, l, r, opts)
+	  opts = opts or {}
+	  opts.buffer = bufnr
+	  vim.keymap.set(mode, l, r, opts)
+	end
+
+	-- Navigation
+	map('n', ']c', function()
+	  if vim.wo.diff then return ']c' end
+	  vim.schedule(function() gs.next_hunk() end)
+	  return '<Ignore>'
+	end, {expr=true})
+
+	map('n', '[c', function()
+	  if vim.wo.diff then return '[c' end
+	  vim.schedule(function() gs.prev_hunk() end)
+	  return '<Ignore>'
+	end, {expr=true})
+
+	-- Actions
+	map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+	map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+	map('n', '<leader>hS', gs.stage_buffer)
+	map('n', '<leader>hu', gs.undo_stage_hunk)
+	map('n', '<leader>hR', gs.reset_buffer)
+	map('n', '<leader>hp', gs.preview_hunk)
+	map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+	map('n', '<leader>tb', gs.toggle_current_line_blame)
+	map('n', '<leader>hd', gs.diffthis)
+	map('n', '<leader>hD', function() gs.diffthis('~') end)
+	map('n', '<leader>td', gs.toggle_deleted)
+
+	-- Text object
+	map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
+}
+
+-- Configure LSP
+require("nvim-lsp-installer").setup{}
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.bashls.setup{}
